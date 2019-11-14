8e980167de5b1bc50ae77b79ab7e50b964e5ead0
LauZyHou <java233@foxmail.com>
2019-11-15 00:41:40 +0800

    add AttackTree

---
+++ include/Model/AttackTree/Attack.hpp
@@ -0,0 +1,24 @@
+﻿//
+//  Attack.hpp
+//
+//  Created by LauZyHou on 2019/11/15.
+//  Copyright © 2019 All rights reserved.
+//
+
+#ifndef Model_Attack_hpp
+#define Model_Attack_hpp
+#include "./AttackNode.hpp"
+
+namespace esc {
+	/// \brief the attack protocal bound to attack nodes or attack trees.
+	class Attack {
+	private:
+		bool enable;
+		AttackNode* originNode;
+		bool isLeaf;
+		bool isFinal;
+		bool isRoot;
+	};
+}
+
+#endif /* Model_Attack_hpp */
---
+++ include/Model/AttackTree/AttackTree.hpp
@@ -0,0 +1,30 @@
+﻿//
+//  AttackTree.hpp
+//
+//  Created by LauZyHou on 2019/11/15.
+//  Copyright © 2019 All rights reserved.
+//
+
+#ifndef Model_AttackTree_hpp
+#define Model_AttackTree_hpp
+#include "./AttackNode.hpp"
+#include "./Attack.hpp"
+#include <string>
+#include <list>
+using std::string;
+using std::list;
+
+namespace esc {
+	/// \brief attack tree protocal.
+	class AttackTree {
+	private:
+		list<AttackNode*> nodes;
+		list<Attack*> attacks;
+
+	public:
+		AttackNode* faultyNode;
+		string errorOfFaultyNode;
+	};
+}
+
+#endif /* Model_AttackTree_hpp */
